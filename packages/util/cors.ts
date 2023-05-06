20e1c7d41d1dc6937bfce52655bc4c7bc2d60095
Felix Schultz <felix.schultz@ca9.io>
2023-05-06 04:22:30 +0200

    Initial Cloudflare Setup

---
+++ packages/util/cors.ts
@@ -0,0 +1,44 @@
+import config from "../../config.json";
+const allowedOrigins = new Set(config.cors);
+
+export class CorsResponse {
+  body: BodyInit | null;
+  status: number;
+  constructor(body: BodyInit | null, status = 200) {
+    this.status = status;
+    this.body = body;
+  }
+
+  finalize(request: Request): Response {
+    let origin = "";
+    if (
+      request.headers.has("Origin") &&
+      allowedOrigins.has(request.headers.get("Origin") || "")
+    )
+      origin = request.headers.get("Origin") || "";
+    const headers: HeadersInit = {
+      "Access-Control-Allow-Origin": origin,
+      "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS, PATCH",
+      "Access-Control-Allow-Headers":
+        "Content-Type, Authorization, X-Requested-With",
+      "Content-Type": "application/json",
+      "X-DNS-Prefetch-Control": "off",
+      "X-Frame-Options": "DENY",
+      "Cache-Control": "no-store",
+      "Content-Security-Policy": "frame-ancestors 'none'",
+    };
+
+    if (request.method === "OPTIONS") {
+      return new Response(null, {
+        status: 204,
+        headers: headers,
+      });
+    }
+
+    const response = new Response(this.body, {
+      status: this.status,
+      headers: headers,
+    });
+    return response;
+  }
+}
---
+++ worker/index-consumer/src/index.ts
@@ -0,0 +1,24 @@
+import { CorsResponse } from "util/cors";
+export interface Env {
+  // Example binding to KV. Learn more at https://developers.cloudflare.com/workers/runtime-apis/kv/
+  // MY_KV_NAMESPACE: KVNamespace;
+  //
+  // Example binding to Durable Object. Learn more at https://developers.cloudflare.com/workers/runtime-apis/durable-objects/
+  // MY_DURABLE_OBJECT: DurableObjectNamespace;
+  //
+  // Example binding to R2. Learn more at https://developers.cloudflare.com/workers/runtime-apis/r2/
+  // MY_BUCKET: R2Bucket;
+  //
+  // Example binding to a Service. Learn more at https://developers.cloudflare.com/workers/runtime-apis/service-bindings/
+  // MY_SERVICE: Fetcher;
+}
+
+export default {
+  async queue(batch: MessageBatch, env: Env, ctx: ExecutionContext) {
+    for (const msg of batch.messages) {
+      // TODO: do something with the message
+      // Explicitly acknowledge the message as delivered
+      msg.ack();
+    }
+  },
+};
